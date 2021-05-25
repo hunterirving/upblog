a6192bf9e18b163bbc831871350091a057b5afa5
Hunter Irving <hunter@hunterirving.com>
2021-05-25 01:26:15 -0400

    Added files.

---
+++ main.js
@@ -0,0 +1,12 @@
+function incrementValue()
+{
+    var value = parseInt(document.getElementById('like_count_value').innerHTML, 10);
+
+		if (isNaN(value)) {
+			value = 1;
+		}
+		else {
+			value ++;
+		}
+    document.getElementById('like_count_value').innerHTML = value;
+}
