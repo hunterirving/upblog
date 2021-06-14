5c9db9803881f20c9c26fafbfedaa25f28826d4b
Hunter Irving <hunter@hunterirving.com>
2021-06-14 00:35:46 -0400

    sticky blog header; animated regobble (thanks Jen <3)

--- site/gobbler/gobbler.js
+++ site/gobbler/gobbler.js
@@ -1,5 +1,14 @@
 function flipGobble()
 {
 	var element = document.getElementById("gobbler_embed");
-	element.classList.toggle("upside_down");
+	if (element.style.transform === '')
+	{
+		element.style.transform = "rotate(0.5turn)";
+	}
+	else
+	{
+		//Get current rotation amount, add 0.5
+		turnAmount = parseFloat(element.style.transform.slice(7).slice(0, -5)) + 0.5;
+		element.style.transform = `rotate(${turnAmount}turn)`;
+	}
 }
