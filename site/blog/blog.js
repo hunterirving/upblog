87fd3ccbceb8f72898272d933293196ffb53481f
Hunter Irving <hunter@hunterirving.com>
2021-07-24 01:07:49 -0400

    changed height to max-height for navbar onClick()

--- site/blog/blog.js
+++ site/blog/blog.js
@@ -8,14 +8,14 @@ function toggleNavLinks()
 	{
 		plus.style.transform = 'rotate(0deg)';
 		nav_links.style.opacity = "0%";
-		nav_links.style.height = "0px";
+		nav_links.style.maxHeight = "0px";
 		nav_links.classList.add('hidden');
 	}
 	else
 	{
 		plus.style.transform = 'rotate(-45deg)';
 		nav_links.style.opacity = "100%";
-		nav_links.style.height = "28px";
+		nav_links.style.maxHeight = "50px";
 		nav_links.classList.remove('hidden');
 	}
 }
