e2c7214e1ae2e72c0c74b24398610fafc1e9c007
Hunter Irving <hunter@hunterirving.com>
2021-08-19 23:29:16 -0400

    increment follower count when Follow button is clicked

--- site/gobbler/gobbler.js
+++ site/gobbler/gobbler.js
@@ -11,3 +11,11 @@ function flipGobble()
 		element.style.transform = `rotate(${turnAmount}turn)`;
 	}
 }
+
+function incrementValue(element_id)
+{
+    var value = parseInt(document.getElementById(element_id).innerHTML, 10);
+    value = isNaN(value) ? 0 : value;
+    value++;
+    document.getElementById(element_id).innerHTML = value;
+}
