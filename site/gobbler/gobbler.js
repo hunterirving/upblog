2a341f1ef68d90805e533802d28dab6be04963e9
Hunter Irving <hunter@hunterirving.com>
2021-08-24 03:04:45 -0400

    added color on action_area hover; dynamic subject/rot13 on mailto links

--- site/gobbler/gobbler.js
+++ site/gobbler/gobbler.js
@@ -1,4 +1,4 @@
-var z = 0;
+var z = 0; //holds current highest z-index
 
 function flipGobble(event)
 {
@@ -21,7 +21,18 @@ function flipGobble(event)
 	}
 }
 
-function findAncestor (el, cls) {
+function incrementActionCount(event)
+{
+	var action_area = findAncestor(event.target, 'action_area');
+	var action_count = action_area.getElementsByClassName('action_count')[0];
+	var value = parseInt(action_count.innerHTML, 10);
+	value = isNaN(value) ? 0 : value;
+	value++;
+	action_count.innerHTML = value;
+}
+
+function findAncestor (el, cls)
+{
     while ((el = el.parentElement) && !el.classList.contains(cls));
     return el;
 }
@@ -34,12 +45,23 @@ function incrementValue(element_id)
     document.getElementById(element_id).innerHTML = value;
 }
 
-function incrementActionCount(event)
+function sendMessage(event)
 {
-	var action_area = findAncestor(event.target, 'action_area');
-	var action_count = action_area.getElementsByClassName('action_count')[0];
-	var value = parseInt(action_count.innerHTML, 10);
-	value = isNaN(value) ? 0 : value;
-	value++;
-	action_count.innerHTML = value;
+	var ancestor = findAncestor(event.target, 'gobble_inner');
+	var gobble_text = ancestor.getElementsByClassName('gobble_inner_2')[0].innerText;
+	var ml2 = rot("znv" + "yg" + "b:");
+	var h = rot("uhagre");
+	var s = rot("uha" + "gre" + "veivat");
+	var ext = rot("p" + "bz");
+	var at = rot("@");
+	var su = rot("f" + "how" + "rpg")
+	window.location.replace(ml2 + h + at + s + "." + ext + "?" + su + "=RE: \"" + gobble_text + "\"");
+}
+
+function rot(str) {
+  var input     = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+  var output    = 'NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm';
+  var index     = x => input.indexOf(x);
+  var translate = x => index(x) > -1 ? output[index(x)] : x;
+  return str.split('').map(translate).join('');
 }
