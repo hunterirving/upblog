6e38d589d27dbb786e26ca892bc318bdf91999b7
Hunter Irving <hunter@hunterirving.com>
2021-09-15 18:36:45 -0400

    added gobbler.py

---
+++ site/gobbler/gobbler.py
@@ -0,0 +1,28 @@
+import fileinput, datetime
+from random import randint
+
+gobble_body = input('Post to Gobbler: ')
+
+newGobble = '''
+				<div class="gobble_prototype">
+					<hr>
+					<div>
+						@hunter
+					</div>
+					<div class="gobble_proto_body">
+						''' + gobble_body + '''
+					</div>
+					<div class="gobble_proto_date">''' + datetime.datetime.now(datetime.timezone.utc).strftime("%a, %d %b %Y %H:%M:%S GMT") + '''
+					</div>
+					<div>
+						<span class="comments">''' + str(randint(10,99)) + '''</span> comments,
+						<span class="regobbles">''' + str(randint(10,99)) + '''</span> regobbles,
+						<span class="likes">''' + str(randint(10,99)) + '''</span> likes
+					</div>
+				</div>
+'''
+
+for line in fileinput.FileInput("index.html",inplace=1):
+    if '<div id="prototype_container">' in line:
+        line=line.replace(line,line + newGobble)
+    print(line, end='')
